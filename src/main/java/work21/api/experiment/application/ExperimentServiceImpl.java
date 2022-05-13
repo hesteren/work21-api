bf67763aa4e9d3d13a6d5e01cc93356dca02dfcb
hesteren <maxvanhesteren@gmail.com>
2022-05-13 11:58:34 +0200

    Added class attributes in service

--- src/main/java/work21/api/experiment/application/ExperimentServiceImpl.java
+++ src/main/java/work21/api/experiment/application/ExperimentServiceImpl.java
@@ -1,4 +1,12 @@
 package work21.api.experiment.application;
 
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import work21.api.experiment.data.ExperimentRepository;
+
+@Service
+@AllArgsConstructor
 public class ExperimentServiceImpl implements ExperimentService {
+
+    private final ExperimentRepository experimentRepository;
 }
