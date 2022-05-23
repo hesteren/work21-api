d1e5164d248c4621638b7a3a1aa25dc52fcb113c
Max <74653870+hesteren@users.noreply.github.com>
2022-05-23 15:42:22 +0200

    Added e.getMessage()

--- src/main/java/work21/api/experiment/presentation/ExperimentController.java
+++ src/main/java/work21/api/experiment/presentation/ExperimentController.java
@@ -27,7 +27,7 @@ public class ExperimentController {
         try {
             return experimentService.create(title, ImageUtility.compressImage(image.getBytes()), sentence, assignment, result, plateau, difficulty, use, successFactor);
         } catch (Exception e) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
         }
     }
 
@@ -39,7 +39,7 @@ public class ExperimentController {
             e.setImage(ImageUtility.decompressImage(e.getImage()));
             return e;
         } catch (Exception e) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
         }
     }
 
@@ -48,7 +48,7 @@ public class ExperimentController {
         try {
             return experimentService.update(id, title, image, sentence, assignment, result, plateau, difficulty, use, successFactor);
         } catch (Exception e) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
         }
     }
 
@@ -66,7 +66,7 @@ public class ExperimentController {
         try {
             return experimentService.index();
         } catch (Exception e) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
         }
     }
 }
